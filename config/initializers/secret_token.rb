af9b97a80e26fd57e87866835c5dc9f3ce370640
Marat <xalmar@gmail.com>
2013-09-24 15:41:22 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mhartl::Application.config.secret_key_base = 'e79f6da633dae8a98ecee0c48ec4a3093926dcb5f3fddb53eb3edd77a8ffd01626de228046bbbd07b8b2e8934bd16d0af544e0c04b08da2ee2703182c8987f56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mhartl::Application.config.session_store :cookie_store, key: '_mhartl_session'
